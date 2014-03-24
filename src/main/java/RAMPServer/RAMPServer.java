42ca081536b1705b5d309ecf87eaf9cd2db5bb9a
Matthew Price <hello@mattprice.me>
2014-03-23 22:29:10 -0400

    Split the byte array into 5 second segments.

--- src/main/java/RAMPServer/RAMPServer.java
+++ src/main/java/RAMPServer/RAMPServer.java
@@ -91,8 +91,47 @@ class EchoHandler implements WebSocketConnectionCallback {
 class StreamingHandler implements WebSocketConnectionCallback {
     @Override
     public void onConnect(WebSocketHttpExchange exchange, WebSocketChannel channel) {
+        // Load the song into a byte buffer.
         File input = new File("build/resources/main/mario.wav");
         byte[] buffer = RAMPServer.toByteArray(input);
-        WebSockets.sendBinary(ByteBuffer.wrap(buffer), channel, null);
+
+        // System.out.println(buffer.length);
+        // 7235522
+
+        // Split the song into 5 second increments.
+        byte[] a = Arrays.copyOfRange(buffer, 0, 452220);
+        byte[] b = Arrays.copyOfRange(buffer, 452220, 452220*2);
+        byte[] c = Arrays.copyOfRange(buffer, 452220*2, 452220*3);
+        byte[] d = Arrays.copyOfRange(buffer, 452220*3, 452220*4);
+        byte[] e = Arrays.copyOfRange(buffer, 452220*4, 452220*5);
+        byte[] f = Arrays.copyOfRange(buffer, 452220*5, 452220*6);
+        byte[] g = Arrays.copyOfRange(buffer, 452220*6, 452220*7);
+        byte[] h = Arrays.copyOfRange(buffer, 452220*7, 452220*8);
+        byte[] i = Arrays.copyOfRange(buffer, 452220*8, 452220*9);
+        byte[] j = Arrays.copyOfRange(buffer, 452220*9, 452220*10);
+        byte[] k = Arrays.copyOfRange(buffer, 452220*10, 452220*11);
+        byte[] l = Arrays.copyOfRange(buffer, 452220*11, 452220*12);
+        byte[] m = Arrays.copyOfRange(buffer, 452220*12, 452220*13);
+        byte[] n = Arrays.copyOfRange(buffer, 452220*13, 452220*14);
+        byte[] o = Arrays.copyOfRange(buffer, 452220*14, 452220*15);
+        byte[] p = Arrays.copyOfRange(buffer, 452220*15, buffer.length);
+
+        // Send each 5 second increment to the browser.
+        WebSockets.sendBinary(ByteBuffer.wrap(a), channel, null);
+        WebSockets.sendBinary(ByteBuffer.wrap(b), channel, null);
+        WebSockets.sendBinary(ByteBuffer.wrap(c), channel, null);
+        WebSockets.sendBinary(ByteBuffer.wrap(d), channel, null);
+        WebSockets.sendBinary(ByteBuffer.wrap(e), channel, null);
+        WebSockets.sendBinary(ByteBuffer.wrap(f), channel, null);
+        WebSockets.sendBinary(ByteBuffer.wrap(g), channel, null);
+        WebSockets.sendBinary(ByteBuffer.wrap(h), channel, null);
+        WebSockets.sendBinary(ByteBuffer.wrap(i), channel, null);
+        WebSockets.sendBinary(ByteBuffer.wrap(j), channel, null);
+        WebSockets.sendBinary(ByteBuffer.wrap(k), channel, null);
+        WebSockets.sendBinary(ByteBuffer.wrap(l), channel, null);
+        WebSockets.sendBinary(ByteBuffer.wrap(m), channel, null);
+        WebSockets.sendBinary(ByteBuffer.wrap(n), channel, null);
+        WebSockets.sendBinary(ByteBuffer.wrap(o), channel, null);
+        WebSockets.sendBinary(ByteBuffer.wrap(p), channel, null);
     }
 }
