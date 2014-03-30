ade0a9a72b0d6c72c1543716cc570b2290439491
Matthew Price <hello@mattprice.me>
2014-03-30 12:27:35 -0400

    Process audio through libPd before streaming.

--- src/main/java/RAMPServer/RAMPServer.java
+++ src/main/java/RAMPServer/RAMPServer.java
@@ -5,7 +5,13 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.ShortBuffer;
 import java.util.Arrays;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
@@ -18,6 +24,7 @@ import io.undertow.websockets.core.WebSocketChannel;
 import io.undertow.websockets.core.WebSockets;
 import io.undertow.websockets.spi.WebSocketHttpExchange;
 import io.undertow.websockets.WebSocketConnectionCallback;
+import org.puredata.core.PdBase;
 
 import static io.undertow.Handlers.path;
 import static io.undertow.Handlers.resource;
@@ -26,25 +33,6 @@ import static io.undertow.Handlers.websocket;
 import javax.sound.sampled.spi.AudioFileWriter;
 
 public class RAMPServer {
-    public static byte[] toByteArray(File file) {
-        int length = (int)file.length();
-        byte[] array = new byte[length];
-
-        try {
-            int offset = 0;
-            InputStream in = new FileInputStream(file);
-            while (offset < length) {
-                int count = in.read(array, offset, (length - offset));
-                offset += count;
-            }
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return array;
-    }
-
     public static void main(final String[] args) {
         // Use port 8080 if $PORT is not set.
         int port = 8080;
@@ -91,47 +79,69 @@ class EchoHandler implements WebSocketConnectionCallback {
 class StreamingHandler implements WebSocketConnectionCallback {
     @Override
     public void onConnect(WebSocketHttpExchange exchange, WebSocketChannel channel) {
-        // Load the song into a byte buffer.
-        File input = new File("build/resources/main/mario.wav");
-        byte[] buffer = RAMPServer.toByteArray(input);
+        AudioInputStream audioInputStream;
+        AudioFormat      audioFormat;
+        float            sampleRate;      // Sample rate in Hz.
+        int              inputChannels;   // Number of input channels.
+        int              outputChannels;  // Number of output channels.
+        int              ticks;           // Number of Pd ticks per buffer.
 
-        // System.out.println(buffer.length);
-        // 7235522
+        // Attempt to load the audio file.
+        try {
+            // Open the Pure Data patch.
+            int patch = PdBase.openPatch("build/resources/main/plain.pd");
 
-        // Split the song into 5 second increments.
-        byte[] a = Arrays.copyOfRange(buffer, 0, 452220);
-        byte[] b = Arrays.copyOfRange(buffer, 452220, 452220*2);
-        byte[] c = Arrays.copyOfRange(buffer, 452220*2, 452220*3);
-        byte[] d = Arrays.copyOfRange(buffer, 452220*3, 452220*4);
-        byte[] e = Arrays.copyOfRange(buffer, 452220*4, 452220*5);
-        byte[] f = Arrays.copyOfRange(buffer, 452220*5, 452220*6);
-        byte[] g = Arrays.copyOfRange(buffer, 452220*6, 452220*7);
-        byte[] h = Arrays.copyOfRange(buffer, 452220*7, 452220*8);
-        byte[] i = Arrays.copyOfRange(buffer, 452220*8, 452220*9);
-        byte[] j = Arrays.copyOfRange(buffer, 452220*9, 452220*10);
-        byte[] k = Arrays.copyOfRange(buffer, 452220*10, 452220*11);
-        byte[] l = Arrays.copyOfRange(buffer, 452220*11, 452220*12);
-        byte[] m = Arrays.copyOfRange(buffer, 452220*12, 452220*13);
-        byte[] n = Arrays.copyOfRange(buffer, 452220*13, 452220*14);
-        byte[] o = Arrays.copyOfRange(buffer, 452220*14, 452220*15);
-        byte[] p = Arrays.copyOfRange(buffer, 452220*15, buffer.length);
+            File input = new File("build/resources/main/mario.wav");
+            audioInputStream = AudioSystem.getAudioInputStream(input);
+            audioFormat = audioInputStream.getFormat();
 
-        // Send each 5 second increment to the browser.
-        WebSockets.sendBinary(ByteBuffer.wrap(a), channel, null);
-        WebSockets.sendBinary(ByteBuffer.wrap(b), channel, null);
-        WebSockets.sendBinary(ByteBuffer.wrap(c), channel, null);
-        WebSockets.sendBinary(ByteBuffer.wrap(d), channel, null);
-        WebSockets.sendBinary(ByteBuffer.wrap(e), channel, null);
-        WebSockets.sendBinary(ByteBuffer.wrap(f), channel, null);
-        WebSockets.sendBinary(ByteBuffer.wrap(g), channel, null);
-        WebSockets.sendBinary(ByteBuffer.wrap(h), channel, null);
-        WebSockets.sendBinary(ByteBuffer.wrap(i), channel, null);
-        WebSockets.sendBinary(ByteBuffer.wrap(j), channel, null);
-        WebSockets.sendBinary(ByteBuffer.wrap(k), channel, null);
-        WebSockets.sendBinary(ByteBuffer.wrap(l), channel, null);
-        WebSockets.sendBinary(ByteBuffer.wrap(m), channel, null);
-        WebSockets.sendBinary(ByteBuffer.wrap(n), channel, null);
-        WebSockets.sendBinary(ByteBuffer.wrap(o), channel, null);
-        WebSockets.sendBinary(ByteBuffer.wrap(p), channel, null);
+            // NOTE: I think that `ticks` can be set to anything we want...
+            sampleRate = audioFormat.getSampleRate();
+            inputChannels = audioFormat.getChannels();
+            outputChannels = audioFormat.getChannels();
+            ticks = audioFormat.getSampleSizeInBits();
+
+            PdBase.openAudio(inputChannels, outputChannels, (int)sampleRate);
+            PdBase.computeAudio(true);
+
+            int sampleSize = 2;
+
+            // Create the buffers used to exchange data between Java and libpd.
+            int frames = PdBase.blockSize() * ticks;
+            short[] pdInBuffer = new short[frames * inputChannels];
+            short[] pdOutBuffer = new short[frames * outputChannels];
+
+            // Create the buffers required for converting from byte[] to short[].
+            byte[] rawInput = new byte[pdInBuffer.length * sampleSize];
+            ShortBuffer rawInputAsShorts = ByteBuffer.wrap(rawInput).asShortBuffer();
+
+            // Create the buffers required for converting from short[] to byte[].
+            byte[] rawOutput = new byte[pdOutBuffer.length * sampleSize];
+            ShortBuffer rawOutputAsShorts = ByteBuffer.wrap(rawOutput).asShortBuffer();
+
+            // audioInputStream.read retuns -1 when there's no more data.
+            int remaining = 1;
+            while (remaining != -1) {
+                // Copy the next song chunk from audioInputStream into rawInput.
+                remaining = audioInputStream.read(rawInput);
+
+                // Convert and place the rawInput (bytes) into pdInBuffer (shorts).
+                rawInputAsShorts.rewind();
+                rawInputAsShorts.get(pdInBuffer);
+
+                // TODO: This returns a status code. Maybe we should check for it?
+                PdBase.process(ticks, pdInBuffer, pdOutBuffer);
+
+                // Convert and place the pdOutBuffer (shorts) into rawOutput (bytes).
+                rawOutputAsShorts.rewind();
+                rawOutputAsShorts.put(pdOutBuffer);
+
+                WebSockets.sendBinaryBlocking(ByteBuffer.wrap(rawOutput), channel);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        }
     }
 }
